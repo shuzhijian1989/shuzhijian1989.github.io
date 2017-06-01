eb9266148b35ddcc2ddea0d9486dba6714659637
shuzhijian1989 <664069640@qq.com>
2017-06-01 20:08:16 +0800

    Signed-off-by: shuzhijian1989 <664069640@qq.com>

---
+++ dist/config/conf.js
@@ -0,0 +1,22 @@
+// SERVER = 'http://192.168.82.221:8888';
+// SERVER = 'https://app.gjyf-qa.com.cn'
+
+
+
+// SERVER = 'http://192.168.82.229:8080';
+// APP_ROUTE = ''
+
+
+
+// SERVER = 'http://192.168.86.89:8170';
+// APP_ROUTE = ''
+
+
+// SERVER = '/mock'
+// APP_ROUTE = ''
+
+// SERVER = 'https://app.gjyf-qa.com.cn';
+// APP_ROUTE = ''
+
+  SERVER = 'https://shuzhijian1989.github.io/demo/'
+  APP_ROUTE = ''
