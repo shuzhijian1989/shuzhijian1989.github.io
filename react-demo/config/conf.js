8d8a7aa7a8e4343df984638ab984401beb4ad779
shuzhijian1989 <664069640@qq.com>
2017-06-02 17:00:35 +0800

    Signed-off-by: shuzhijian1989 <664069640@qq.com>

---
+++ react-demo/config/conf.js
@@ -0,0 +1,20 @@
+// SERVER = 'http://192.168.82.221:8888';
+// SERVER = 'https://app.gjyf-qa.com.cn'
+
+
+
+// SERVER = 'http://192.168.82.229:8080';
+// APP_ROUTE = ''
+
+
+
+
+// SERVER = 'http://192.168.86.89:8170';
+// APP_ROUTE = ''
+
+SERVER = '/dist/mock'
+APP_ROUTE = ''
+
+// SERVER = 'https://app.gjyf-qa.com.cn';
+// APP_ROUTE = ''
+ 
