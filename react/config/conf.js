220aa06ded182bd9c5a45273ad4337fd1a9737d8
shuzhijian1989 <664069640@qq.com>
2017-07-15 11:48:23 +0800

    Signed-off-by: shuzhijian1989 <664069640@qq.com>

--- react/config/conf.js
+++ react/config/conf.js
@@ -1,7 +1,7 @@
 // SERVER = 'https://app.gjyf-qa.com.cn';
-// SERVER =  'https://app.gjyf-qa.com.cn/bdpes2api'
+// SERVER =  'https://app.gjyf-qa.com.cn/bdpes2api'   //  真实测试环境
 // SERVER =  'http://192.168.86.89:8170/bdpes2api/'
-SERVER =  'https://rap.gjyf.com.cn/mockjsdata/2'
+SERVER =  'https://rap.gjyf.com.cn/mockjsdata/2'     //  mock  环境
 
 // SERVER = '/mock'
 
